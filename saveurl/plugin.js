13ed3ec0eeb95018ffe6ba93b07f74d9bc6021da
Mildred Ki'Lya <mildred-pub.git@mildred.fr>
2015-01-24 12:26:20 +0100

    Refactoring

--- saveurl/plugin.js
+++ saveurl/plugin.js
@@ -4,24 +4,42 @@
     hidpi: true,
     init: function( editor ) {
 
+        /***********************************
+        ** Warn before closing the window **
+        ***********************************/
+        
+        window.addEventListener("beforeunload", function (e) {
+            if(!editor.checkDirty()) {
+                console.log("Editor is clean.");
+                return;
+            }
+            var confirmationMessage = "If you leave this page, you'll loose unsaved changes.";
+            console.log(confirmationMessage);
+            (e || window.event).returnValue = confirmationMessage;     //Gecko + IE
+            return confirmationMessage;                                //Gecko + Webkit, Safari, Chrome etc.
+        });
+
+        /**************
+        ** Auto Save **
+        **************/
+
         var autoSaveDelay = 1000;
         var autoSaveAlready = false;
         var autoSaveEnabled = true;
         var autoSaveKey = editor.config.autosave_SaveKey != null ? editor.config.autosave_SaveKey : 'autosave_' + window.location + "_" + editor.id;
         
         function autoSave() {
-            if(!autoSaveEnabled) return false;
+            if(!autoSaveEnabled) return {ok: false};
             if(!autoSaveAlready && localStorage.getItem(autoSaveKey)) {
-                console.log("Another editor auto saved before us");
-                return false;
+                return {ok: false, msg: "Not Saved: another editor is open"};
             }
             try {
                 var compressedJSON = LZString.compressToUTF16(JSON.stringify({ data: editor.getSnapshot(), saveTime: new Date() }));
                 autoSaveAlready = true;
                 localStorage.setItem(autoSaveKey, compressedJSON);
-                return true;
+                return {ok: true};
             } catch(e) {
-                return false;
+                return {ok: false};
             }
         }
         
@@ -48,10 +66,112 @@
             }
         }
         
+        var dirty = false;
+        editor.on('change', function (ev) {
+            if(dirty) return;
+            if(!editor.checkDirty()) return;
+            dirty = true;
+            
+            showStatus("Not Saved")
+
+            setTimeout(function(){
+                var saved = autoSave();
+                if(!saved.ok) {
+                    showStatus(saved.msg);
+                    return;
+                }
+
+                dirty = false;
+                showStatus("Saved in memory");
+            }, autoSaveDelay);
+        });
+        
+        /*******************
+        ** Status Message **
+        *******************/
+        
         editor.on('uiSpace', function (event) {
             if (event.data.space != 'bottom') return;
             event.data.html += '<div class="autoSaveMessage" unselectable="on"><div unselectable="on" class="hidden" id="cke_saveurlMessage_' + editor.name + '"></div></div>';
         }, editor, null, 100);
+        
+        function showStatus(msg, timeout) {
+            var autoSaveMessage = document.getElementById('cke_saveurlMessage_' + editor.name);
+            if (autoSaveMessage) {
+                autoSaveMessage.className = "show";
+                autoSaveMessage.textContent = msg;
+                if(timeout) {
+                    setTimeout(function() {
+                        autoSaveMessage.className = "hidden";
+                    }, timeout);
+                }
+            }
+
+        }
+        
+        function hideStatus() {
+            var autoSaveMessage = document.getElementById('cke_saveurlMessage_' + editor.name);
+            if (autoSaveMessage) {
+                autoSaveMessage.className = "hidden";
+                autoSaveMessage.textContent = "";
+            }
+        }
+        
+        /*****************
+        ** HTTP Request **
+        *****************/
+        
+        // opts.url
+        // opts.content_type
+        // opts.data
+        // cb(ok, location, xhr)
+        function httpPutFile(opts, cb) {
+            var url = opts.url;
+            var xhr = new XMLHttpRequest();
+            xhr.open('PUT', url, true);
+            xhr.onreadystatechange = httpCallback;
+            xhr.setRequestHeader('Content-Type', opts.content_type);
+            xhr.send(opts.data);
+            
+            function httpCallback() {
+                if ( xhr.readyState != 4 ) return;
+                var ok = (xhr.status >= 200 && xhr.status < 300) ||
+                    xhr.status == 304 || xhr.status === 0 || xhr.status == 1223;
+                
+                var location = xhr.getResponseHeader("Location");
+                if(!location) {
+                    location = url;
+                } else if(location[0] == '/') {
+                    location = url.replace(/^(.*[^\/])?\/[^\/].*$/, "$1" + location);
+                }
+                
+                cb(ok, location, xhr);
+            }
+        }
+
+        function httpPutEditor(url, cb) {
+            httpPutFile({
+                url:          url,
+                data:         editor.getData(),
+                content_type: 'text/html; charset=UTF-8'
+            }, function(ok, location, xhr){
+                if(ok) {
+                    editor.documentURL = location;
+                    if(location != url) {
+                        alert("Document saved to a new location:\n" + location);
+                    }
+                    autoSaveReset();
+                    showStatus("Saved at " + location);
+                } else {
+                    alert("Could not save document:\n" + xhr.responseText);
+                }
+                cb();
+            });
+        }
+        
+        /****************
+        ** Open Dialog **
+        ****************/
 
 	CKEDITOR.dialog.add( 'openUrl', function( editor ) {
             var dialogDefinition = {
@@ -103,6 +223,10 @@
             return dialogDefinition;
             
         } );
+        
+        /****************
+        ** Save Dialog **
+        ****************/
 
 	CKEDITOR.dialog.add( 'saveUrl', function( editor ) {
             return {
@@ -150,47 +274,16 @@
                     }
                     
                     ev.data.hide = false;
-                    
-                    var xhr = new XMLHttpRequest();
-                    xhr.open('PUT', url, true);
-                    xhr.onreadystatechange = function() {
-                        if ( xhr.readyState == 4 ) {
-                            if( (xhr.status >= 200 && xhr.status < 300) ||
-                                xhr.status == 304 || xhr.status === 0 || xhr.status == 1223 ) {
-                                var location = xhr.getResponseHeader("Location");
-                                if(location) {
-                                    if(location[0] == '/') {
-                                        location = document.location.origin + location;
-                                    }
-                                    if(editor.documentURL != location) {
-                                        editor.documentURL = location;
-                                        alert("Document saved to a new location:\n" + location);
-                                    }
-                                } else {
-                                    location = url;
-                                }
-                                autoSaveReset();
-                                
-                                var autoSaveMessage = document.getElementById('cke_saveurlMessage_' + editor.name);
-                                if (autoSaveMessage) {
-                                    autoSaveMessage.className = "show";
-                                    autoSaveMessage.textContent = "Saved at " + location;
-                                    /*setTimeout(function() {
-                                        autoSaveMessage.className = "hidden";
-                                    }, 2000);*/
-                                }
-                            } else {
-                                alert("Could not save document:\n" + xhr.responseText);
-                            }
-                            dlg.hide();
-                        }
-                    };
-                    xhr.setRequestHeader('Content-type', 'text/html; charset=UTF-8');
-                    xhr.send(editor.getData());
+                    httpPutEditor(url, function(){
+                        dlg.hide();
+                    });
                 }
             };
         } );
         
+        /*************
+        ** Commands **
+        *************/
         
         editor.addCommand( 'openUrl', new CKEDITOR.dialogCommand( 'openUrl' ) );
         editor.addCommand( 'saveUrlAs', new CKEDITOR.dialogCommand( 'saveUrl' ) );
@@ -209,6 +302,10 @@
             icon: 'openurl'
         });
         
+        /*******************
+        ** Initialization **
+        *******************/
+        
         editor.on('instanceReady', function (ev) {
             CKEDITOR.scriptLoader.load(CKEDITOR.getUrl(CKEDITOR.plugins.getPath('saveurl') + 'js/extensions.min.js'), function(){
                 autoSaveRestore();
@@ -222,39 +319,5 @@
             });
         });
         
-        var dirty = false;
-        editor.on('change', function (ev) {
-            if(dirty) return;
-            if(!editor.checkDirty()) return;
-            var autoSaveMessage = document.getElementById('cke_saveurlMessage_' + editor.name);
-            dirty = true;
-                  
-            if (autoSaveMessage) {
-                autoSaveMessage.className = "show";
-                autoSaveMessage.textContent = "Not Saved";
-            }
-
-            setTimeout(function(){
-                if(!autoSave()) return;
-
-                dirty = false;
-                if (autoSaveMessage) {
-                    autoSaveMessage.className = "show";
-                    autoSaveMessage.textContent = "Saved in memory";
-                }
-            }, autoSaveDelay);
-        });
-        
-        window.addEventListener("beforeunload", function (e) {
-            if(!editor.checkDirty()) {
-                console.log("Editor is clean.");
-                return;
-            }                
-            var confirmationMessage = "If you leave this page, you'll loose unsaved changes.";
-            console.log(confirmationMessage);
-            (e || window.event).returnValue = confirmationMessage;     //Gecko + IE
-            return confirmationMessage;                                //Gecko + Webkit, Safari, Chrome etc.
-        });
-
     }
 });
